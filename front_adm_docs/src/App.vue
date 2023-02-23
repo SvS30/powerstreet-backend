fc7fe36f5803bc417bdf28752173f4a443723f79
salimv <salimvzqz@gmail.com>
2023-02-23 02:44:39 -0600

    Frontend Adm Docs init

---
+++ front_adm_docs/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <app-header />
+    <sidebar-menu />
+    <router-view />
+    <app-footer />
+  </div>
+</template>
+
+<script>
+import AppHeader from "@/components/AppHeader.vue";
+import AppFooter from "@/components/AppFooter.vue";
+import SidebarMenu from "@/components/SidebarMenu.vue";
+
+export default {
+  components: {
+    AppHeader,
+    AppFooter,
+    SidebarMenu
+  },
+};
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
