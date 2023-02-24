a053fb9c34d23342ea72a155cf906e5ab974ae62
salimv <salimvzqz@gmail.com>
2023-02-24 11:54:41 -0600

    Feature: Upload files added

--- front_adm_docs/src/views/AboutView.vue
+++ front_adm_docs/src/views/AboutView.vue
@@ -3,22 +3,28 @@
     <h1>This is an about page</h1>
     <Card heightCard="100px">
       <div class="btn-group">
-        <Button type="btn-secondary" iconName="bx bx-cloud-upload" textButton="Cargar archivo" />
-        <Button type="btn-warning" iconName="bx bxs-edit-alt" textButton="Editar" @click="changeStatusEditing"/>
+        <input id="fileUpload" type="file" ref="doc" hidden="true" @change="readFile">
+        <Button type="btn-secondary" iconName="bx bx-cloud-upload" textButton="Cargar archivo"
+          @click="chooseFile"/>
+        <Button type="btn-warning" iconName="bx bxs-edit-alt" textButton="Editar"
+          @click="changeStatusEditing" />
         <Button type="btn-primary" iconName="bx bxs-save" textButton="Guardar" />
         <Button type="btn-danger" iconName="bx bx-undo" textButton="Cancelar" />
       </div>
+      <div v-if="errorInFile" class="item error">
+        <h4>{{ errorMessage }}</h4>
+      </div>
     </Card>
     <Card heightCard="400px">
-      <VueShowdown v-if="isEditing == false" markdown="# Hello, world!" />
+      <VueShowdown v-if="isEditing == false" :markdown="contentFile" />
       <textarea v-else></textarea>
     </Card>
     <Card heightCard="150px">
       <div class="wrapper">
-      <h4 class="detail">Autor: John Doe</h4>
-      <h4 class="detail">Creado: 2023-02-23</h4>
-      <h4 class="detail">Ultima modificación: 2023-02-23 por Steven Hank</h4>
-    </div>
+        <h4 class="detail">Autor: John Doe</h4>
+        <h4 class="detail">Creado: 2023-02-23</h4>
+        <h4 class="detail">Ultima modificación: 2023-02-23 por Steven Hank</h4>
+      </div>
     </Card>
   </div>
 </template>
@@ -35,12 +41,36 @@ export default {
   },
   data() {
     return {
-      isEditing: false
+      isEditing: false,
+      file: null,
+      contentFile: null,
+      errorInFile: false,
+      errorMessage: null
     }
   },
   methods: {
     changeStatusEditing() {
       this.isEditing = !this.isEditing;
+    },
+    chooseFile() {
+      this.errorInFile = false;
+      document.getElementById('fileUpload').click();
+    },
+    readFile() {
+      this.file = this.$refs.doc.files[0];
+      const reader = new FileReader();
+      if (this.file.name.includes(".md")) {
+        reader.onload = (data) => this.contentFile = data.target.result;
+        reader.onerror = (err) => {
+          console.log(err);
+          this.errorMessage = "An error occurred while processing the file content";
+          this.errorInFile = true;
+        }
+        reader.readAsText(this.file);
+      } else {
+        this.errorMessage = "This file extension is not supported!";
+        this.errorInFile = true;
+      }
     }
   }
 }
@@ -63,5 +93,23 @@ export default {
 
 .detail {
   width: 100%;
+}
+
+.item {
+  width: 350px;
+  margin: 5px auto 10px auto;
+  padding: 10px 20px;
+  color: white;
+  border-radius: 5px;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 3px 0px,
+    rgba(0, 0, 0, 0.06) 0px 1px 2px 0px;
+}
+
+.error {
+  background-color: rgb(204, 41, 13);
+}
+
+#fileUpload {
+  display: none;
 }
 </style>
